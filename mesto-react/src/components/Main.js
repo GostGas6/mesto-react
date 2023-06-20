994f5484d1a93ffa4e572b237fb4999248f4f6f7
GostGas6 <you@example.com>
2023-06-20 13:29:16 +0300

    Интегрировал popup

--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -1,17 +1,28 @@
 import avatar from '../images/avatar.png';
 
 function Main() {
+
+    function handleEditProfileClick() {
+        document.querySelector('#popup_edit').classList.add('popup_opened')
+    }
+    function handleEditAvatarClick() {
+        document.querySelector('#popup-change-avatar').classList.add('popup_opened')
+    }
+    function handleAddPlaceClick() {
+        document.querySelector('#popup-add').classList.add('popup_opened')
+    }
+
     return ((
         <main>
             <section className="profile">
-                <div className="profile__avatar-overlay"></div>
+                <div onClick={handleEditAvatarClick} className="profile__avatar-overlay"></div>
                 <img className="profile__avatar" src={avatar} alt="Аватар профиля" />
                 <div className="profile__info">
                     <h1 className="profile__title">Жак-Ив Кусто</h1>
-                    <button aria-label="Edit profile" className="profile__edit-button" type="button"></button>
+                    <button onClick={handleEditProfileClick} aria-label="Edit profile" className="profile__edit-button" type="button"></button>
                     <p className="profile__subtitle">Исследователь океана</p>
                 </div>
-                <button aria-label="Add" className="profile__add-button" type="button"></button>
+                <button onClick={handleAddPlaceClick} aria-label="Add" className="profile__add-button" type="button"></button>
             </section>
             <section className="elements">
             </section>
