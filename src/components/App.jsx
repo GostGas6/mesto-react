4f46505d4b1b2704d4b393eeafe518ab87dcce25
GostGas6 <you@example.com>
2023-06-25 12:48:31 +0300

    2 iter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,6 +38,7 @@ function App() {
     setIsDeleteMestoPopupOpen(true)
   }
 
+
   return (
     <>
 
--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -6,17 +6,13 @@ export default function ImagePopup({
     const isOpen = card.link ? 'popup_opened' : ''
 
     return (
-        <div
-            className={`popup popup_type_${popupType} 
-        ${isOpen ? 'popup_opened' : ''}`
-            } id="showMesto"
+        <div className={`popup popup-image ${isOpen ? 'popup_opened' : ''}`
+        } id="popup_image"
         >
-            <div className="popup popup-image" id="popup_image">
-                <div className="popup__container-image">
-                    <button onClick={onClose} aria-label="Close image" className="popup__close-button" id="popup_close-image" type="button"></button>
-                    <img className="popup__image" src={card.link} alt={card.name} />
-                    <h2 className="popup__text">{card.name}</h2>
-                </div>
+            <div className="popup__container-image">
+                <button onClick={onClose} aria-label="Close image" className="popup__close-button" id="popup_close-image" type="button"></button>
+                <img className="popup__image" src={card.link} alt={card.name} />
+                <h2 className="popup__text">{card.name}</h2>
             </div>
         </div>
     )
